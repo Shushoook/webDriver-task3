115795a21ad903822f045f246b5baa5b7d9ed45e
Shushoook <shushoook6666@gmail.com>
2023-10-18 17:50:19 +0400

    Task3

--- src/test/java/com/epam/engx/selenium/pages/google/SearchResultPage.java
+++ src/test/java/com/epam/engx/selenium/pages/google/SearchResultPage.java
@@ -2,14 +2,21 @@ package com.epam.engx.selenium.pages.google;
 
 import com.epam.engx.selenium.pages.browser.Page;
 import com.epam.engx.selenium.pages.model.Link;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public final class SearchResultPage extends Page {
-    @FindBy(css = "div.gs-webResult div.gsc-thumbnail-inside a.gs-title")
+    @FindBy(xpath = "//div[contains(@class, 'gsc-result')]//div[contains(@class, 'gsc-thumbnail-inside')]")
     private List<WebElement> results;
 
     @FindBy(css = "a:first-child > b")
@@ -20,6 +27,8 @@ public final class SearchResultPage extends Page {
     }
 
     public List<Link> links() {
+        FluentWait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(20), Duration.ofMillis(500)).ignoring(StaleElementReferenceException.class);
+        wait.until(ExpectedConditions.visibilityOfAllElements(results));
         return results.stream().map(Link::new).toList();
     }
 
